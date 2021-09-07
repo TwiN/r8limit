9313c756064ab9f82946aed12055334536fd230b
TwinProduction <twin@twinnation.org>
2021-09-07 19:34:05 -0400

    Add gradual refilling policy

--- README.md
+++ README.md
@@ -24,3 +24,63 @@ fn main() {
     println!("{}", limiter.attempt()); // false
 }
 ```
+
+## Refill policies
+The refill policy is what determines how the number of executions in an interval are refilled.
+
+### Full
+By default, the refill policy is set to `Full`. This means that every time the difference between now and the
+start of the current window has reached or exceeded the specified interval, the number of executions is reset to
+the maximum number of executions allowed during the interval.
+
+```rust
+use std::time::Duration;
+use std::thread::sleep;
+use crate::RateLimiter;
+
+fn main() {
+    let mut limiter = RateLimiter::new(3, Duration::from_secs(1));
+    limiter.attempt(); // returns true; executions remaining in window: 2
+    limiter.attempt(); // returns true; executions remaining in window: 1
+    limiter.attempt(); // returns true; executions remaining in window: 0
+    limiter.attempt(); // returns false; executions remaining in window: 0
+    // Remember that the interval is set to 1s
+    sleep(Duration::from_millis(500)); // executions remaining in window: 0
+    // As you can see, even though half of the interval has passed, there are still 0 executions available.
+    assert_eq!(limiter.attempt(), false); // returns false; executions remaining for window: 0
+    // That is what the default refill policy, RefillPolicy::Full, does.
+    // We'll sleep for the remainder of the window, which means that the next attempt will reset the window
+    sleep(Duration::from_millis(500)); // executions remaining in window: 3
+    limiter.attempt(); // returns true; executions remaining in window: 2
+    limiter.attempt(); // returns true; executions remaining in window: 1
+    limiter.attempt(); // returns true; executions remaining in window: 0
+    limiter.attempt(); // returns false; executions remaining in window: 0
+}
+```
+
+### Gradual
+Unlike the `Full` refill policy, the `Gradual` refill policy does not wait until the interval has completely elapsed
+to refill the number of executions remaining. 
+
+```rust
+use std::time::Duration;
+use std::thread::sleep;
+use crate::{RateLimiter, RefillPolicy};
+
+fn main() {
+    let mut limiter = RateLimiter::new(2, Duration::from_millis(500)).with_refill_policy(RefillPolicy::Gradual);
+    limiter.attempt(); // returns true; executions remaining in window: 1
+    limiter.attempt(); // returns true; executions remaining in window: 0
+    limiter.attempt(); // returns false; executions remaining in window: 0
+    // The Gradual refill policy calculates the percentage of the interval which has 
+    // elapsed, and multiplies that by the number of executions allowed per interval.
+    // This means that if we wait for half of the interval, half of the executions will become available again
+    sleep(Duration::from_millis(250)); // executions remaining in window: 1
+    limiter.attempt(); // returns true; executions remaining in window: 0
+    limiter.attempt(); // returns false; executions remaining in window: 0
+    sleep(Duration::from_millis(500)); // executions remaining in window: 2
+    limiter.attempt(); // returns true; executions remaining in window: 1
+    limiter.attempt(); // returns true; executions remaining in window: 0
+    limiter.attempt(); // returns false; executions remaining in window: 0
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -21,57 +21,127 @@
 //! ```
 
 use std::time::{Duration, Instant};
+use crate::RefillPolicy::Full;
 
 pub struct RateLimiter {
-    max_executions_per_window: u64,
-    executions_left_in_window: u64,
+    max_executions_per_window: f64,
+    executions_left_in_window: f64,
     window_start_time: Instant,
     window_duration: Duration,
+    refill_policy: RefillPolicy
 }
 
 impl RateLimiter {
     pub fn new(max_executions_per_internal: u64, interval: Duration) -> RateLimiter {
         RateLimiter {
-            max_executions_per_window: max_executions_per_internal,
-            executions_left_in_window: max_executions_per_internal,
+            max_executions_per_window: max_executions_per_internal as f64,
+            executions_left_in_window: max_executions_per_internal as f64 ,
             window_start_time: Instant::now(),
             window_duration: interval,
+            refill_policy: Full
         }
     }
 
+    pub fn with_refill_policy(mut self, refill_policy: RefillPolicy) -> RateLimiter {
+        self.refill_policy = refill_policy;
+        self
+    }
+
     pub fn attempt(&mut self) -> bool {
-        if self.window_start_time.elapsed() > self.window_duration {
-            self.window_start_time = Instant::now();
-            self.executions_left_in_window = self.max_executions_per_window;
+        match &self.refill_policy {
+            RefillPolicy::Full => {
+                if self.window_start_time.elapsed() > self.window_duration {
+                    self.window_start_time = Instant::now();
+                    self.executions_left_in_window = self.max_executions_per_window;
+                }
+            },
+            RefillPolicy::Gradual => {
+                if self.executions_left_in_window != self.max_executions_per_window {
+                    if self.window_start_time.elapsed() > self.window_duration {
+                        // Been a while since the rate limiter was triggered, so we'll reset everything
+                        self.window_start_time = Instant::now();
+                        self.executions_left_in_window = self.max_executions_per_window;
+                    } else {
+                        // Need to move window and update the executions_left_in_window
+                        let percentage_done = self.window_start_time.elapsed().as_secs_f64()/self.window_duration.as_secs_f64();
+                        println!("self.executions_left_in_window={}", self.executions_left_in_window);
+                        let executions_due = percentage_done * self.max_executions_per_window;
+                        self.window_start_time = Instant::now();
+                        self.executions_left_in_window += executions_due;
+                        println!("executions_due={}; self.executions_left_in_window={}", executions_due, self.executions_left_in_window);
+                    }
+                }
+            }
         }
-        if self.executions_left_in_window == 0 {
+        if self.executions_left_in_window < 1.0 {
             return false;
         }
-        self.executions_left_in_window -= 1;
+        self.executions_left_in_window -= 1.0;
         return true;
     }
+}
+
+pub enum RefillPolicy {
+    Full,
+    Gradual
 }
 
 #[cfg(test)]
 mod tests {
     use std::time::Duration;
     use std::thread::sleep;
-    use crate::RateLimiter;
+    use crate::{RateLimiter, RefillPolicy};
 
     #[test]
-    fn it_works() {
+    fn rate_limiter_full() {
         let mut limiter = RateLimiter::new(3, Duration::from_secs(1));
+        assert_eq!(limiter.attempt(), true); // executions remaining for window: 2
+        assert_eq!(limiter.attempt(), true); // executions remaining for window: 1
+        assert_eq!(limiter.attempt(), true); // executions remaining for window: 0
+        assert_eq!(limiter.attempt(), false); // executions remaining for window: 0
+        // Remember that the interval is set to 1s
+        sleep(Duration::from_millis(500)); // executions remaining for window: 0
+        // As you can see, even though half of the interval has passed, there are still 0 executions available.
+        assert_eq!(limiter.attempt(), false);
+        // That is what the default refill policy, RefillPolicy::Full, does.
+        // We'll sleep for the remainder of the window
+        sleep(Duration::from_millis(500)); // executions remaining for window: 3
+        assert_eq!(limiter.attempt(), true); // executions remaining for window: 2
+        assert_eq!(limiter.attempt(), true); // executions remaining for window: 1
+        assert_eq!(limiter.attempt(), true); // executions remaining for window: 0
+        assert_eq!(limiter.attempt(), false); // executions remaining for window: 0
+        assert_eq!(limiter.attempt(), false); // executions remaining for window: 0
+    }
+
+    #[test]
+    fn rate_limiter_gradual() {
+        let mut limiter = RateLimiter::new(4, Duration::from_millis(500)).with_refill_policy(RefillPolicy::Gradual);
+        assert_eq!(limiter.attempt(), true);
         assert_eq!(limiter.attempt(), true);
         assert_eq!(limiter.attempt(), true);
         assert_eq!(limiter.attempt(), true);
         assert_eq!(limiter.attempt(), false);
-        assert_eq!(limiter.attempt(), false);
-        assert_eq!(limiter.attempt(), false);
-        sleep(Duration::from_secs(1));
-        assert_eq!(limiter.attempt(), true);
+        sleep(Duration::from_millis(250));
         assert_eq!(limiter.attempt(), true);
         assert_eq!(limiter.attempt(), true);
         assert_eq!(limiter.attempt(), false);
+        sleep(Duration::from_millis(500));
+        assert_eq!(limiter.attempt(), true);
+        assert_eq!(limiter.attempt(), true);
+        assert_eq!(limiter.attempt(), true);
+        assert_eq!(limiter.attempt(), true);
+        assert_eq!(limiter.attempt(), false);
+        sleep(Duration::from_millis(125));
+        assert_eq!(limiter.attempt(), true);
+        assert_eq!(limiter.attempt(), false);
+        sleep(Duration::from_millis(125));
+        assert_eq!(limiter.attempt(), true);
+        assert_eq!(limiter.attempt(), false);
+        sleep(Duration::from_millis(750));
+        assert_eq!(limiter.attempt(), true);
+        assert_eq!(limiter.attempt(), true);
+        assert_eq!(limiter.attempt(), true);
+        assert_eq!(limiter.attempt(), true);
         assert_eq!(limiter.attempt(), false);
     }
 }
